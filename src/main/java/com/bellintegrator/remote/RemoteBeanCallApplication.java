565940050bf1268e42b539302302e9c524659585
oleg <oleg@acer-aspire-3820>
2022-04-14 11:09:14 +0300

    first commit

---
+++ src/main/java/com/bellintegrator/remote/RemoteBeanCallApplication.java
@@ -0,0 +1,13 @@
+package com.bellintegrator.remote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteBeanCallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemoteBeanCallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bellintegrator/remote/RemoteBeanCallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bellintegrator.remote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemoteBeanCallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
